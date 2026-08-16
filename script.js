f3c858384c6e7a7a0e5822c264b6b857bf330fd8
danielpretin <danielstroeher06@gmail.com>
2026-08-15 22:51:43 -0300

    Add calculator logic winth JavaScript

--- script.js
+++ script.js
@@ -4,10 +4,23 @@ let display = document.getElementById("display");
 
 let botoes = document.querySelectorAll("button");
 
-botoes.forEach(function(botao){
-    botao.addEventListener("click", function(){
-        console.log("clicou em: " + botao.textContent);
+botoes.forEach(function (botao) {
+    botao.addEventListener("click", function () {
+        let valor = botao.textContent;
 
+        if (valor == "C") {
+            display.textContent = "0";
+        } else if (valor == "=") {
+            let expressao = display.textContent;
+            expressao = expressao.replace("x", "*");
+            display.textContent = eval(expressao);
+        } else {
+            if (display.textContent == "0") {
+                display.textContent = valor;
+            }else{
+                display.textContent += valor;
+            }
+        }
     });
 });
 
