83817f291dd3cbb7f7dbb7896d3b8066d2987604
danielpretin <danielstroeher06@gmail.com>
2026-08-15 23:37:53 -0300

    Add scientific buttons functionality (%, CE, 1/x, x², √x, +/-)

--- script.js
+++ script.js
@@ -21,6 +21,19 @@ botoes.forEach(function (botao) {
             } else {
                 display.textContent = "0";
             }
+        } else if (valor == "CE") {
+            display.textContent = "0";
+        } else if (valor == "%") {
+            display.textContent = eval(display.textContent) / 100;
+        } else if (valor == "1/x") {
+            display.textContent = 1 / eval(display.textContent);
+        } else if (valor == "x²") {
+            let num = eval(display.textContent);
+            display.textContent = num * num;
+        } else if (valor == "√x") {
+            display.textContent = Math.sqrt(eval(display.textContent));
+        } else if (valor == "+/-") {
+            display.textContent = eval(display.textContent) * -1;
         } else {
             if (display.textContent == "0") {
                 display.textContent = valor;
